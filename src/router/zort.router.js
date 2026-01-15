ff0fefea0af86c8e4c3bbcde4a0bef3aa6ccc63b
Nopparat Munsuwan <nopparat.mun@onetwotrading.co.th>
2026-01-16 00:04:23 +0700

    update

--- src/router/zort.router.js
+++ src/router/zort.router.js
@@ -1,17 +1,47 @@
 export default {
-    path: '/onlineManage',
-    component: () => import('../views/zort/layout.vue'),
-    redirect: '/onlineManage/dashboard',
-    children: [
-        { path: '/onlineManage/dashboard', component: () => import('../views/zort/dashboard.vue') },
-        { path: '/onlineManage/order/all', component: () => import('../views/zort/orderZortAll.vue') },
-        { path: '/onlineManage/order/inv', component: () => import('../views/zort/orderZort.vue') },
-        { path: '/onlineManage/order/reciept', component: () => import('../views/zort/orderZort.vue') },
-        { path: '/onlineManage/order/history', component: () => import('../views/zort/orderZortHistory.vue') },
-        { path: '/onlineManage/order/reprint', component: () => import('../views/zort/orderZortReprint.vue') },
-        { path: '/onlineManage/order/erp', component: () => import('../views/zort/orderZortErp.vue') },
-        { path: '/onlineManage/stock/zort', component: () => import('../views/zort/stockZort.vue') },
-        { path: '/onlineManage/stock/erp', component: () => import('../views/zort/stockERP.vue') },
-        { path: '/onlineManage/order/summary', component: () => import('../views/zort/orderSummaryAll.vue')}
-    ]
-};
+  path: '/onlineManage',
+  component: () => import('../views/zort/layout.vue'),
+  redirect: '/onlineManage/dashboard',
+  children: [
+    {
+      path: 'dashboard',
+      component: () => import('../views/zort/dashboard.vue')
+    },
+    {
+      path: 'order/all',
+      component: () => import('../views/zort/orderZortAll.vue')
+    },
+    {
+      path: 'order/inv',
+      component: () => import('../views/zort/orderZort.vue')
+    },
+    {
+      path: 'order/reciept',
+      component: () => import('../views/zort/orderZort.vue')
+    },
+    {
+      path: 'order/history',
+      component: () => import('../views/zort/orderZortHistory.vue')
+    },
+    {
+      path: 'order/reprint',
+      component: () => import('../views/zort/orderZortReprint.vue')
+    },
+    {
+      path: 'order/erp',
+      component: () => import('../views/zort/orderZortErp.vue')
+    },
+    {
+      path: 'stock/zort',
+      component: () => import('../views/zort/stockZort.vue')
+    },
+    {
+      path: 'stock/erp',
+      component: () => import('../views/zort/stockERP.vue')
+    },
+    {
+      path: 'order/summary',
+      component: () => import('../views/zort/orderSummaryAll.vue')
+    }
+  ]
+}
