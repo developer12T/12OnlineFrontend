ba33a9550434148dd42b827332c02cbe66d28a3b
Nopparat Munsuwan <nopparat.mun@onetwotrading.co.th>
2026-01-16 16:18:06 +0700

    update

--- src/stores/modules/auth.js
+++ src/stores/modules/auth.js
@@ -22,8 +22,8 @@ export const useAuthStore = defineStore("auth", {
     async login(userLogin, passwordLogin) {
       try {
         const response = await axios.post(
-          // import.meta.env.VITE_API_BASE_URL + "/12lmsapi/api/auth/login",
-          'http://192.168.2.97:8383' + "/12Trading/authen",
+          import.meta.env.VITE_API_BASE_URL + "/12lmsapi/api/auth/login",
+          // 'http://192.168.2.97:8383' + "/12Trading/authen",
           { username: userLogin, password: passwordLogin },
           {
             headers: { "Content-Type": "application/json" },
